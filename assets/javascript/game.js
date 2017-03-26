70e111d7ecee6934608d5bc6b6bea8f05b02ea36
TREX Hughes <TREX@Traviss-MacBook-Pro-2.local>
2017-03-25 21:45:56 -0500

    got the javascript working with the exception of listing guessed letters in an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,10 @@
 // This is the alphabet array and the variable that guesses the
-// computer's letter
+// computer's letter and defines the initial values for the 
+// guesses
 
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
 	"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", 
 	"W", "X", "Y", "Z"];
-
-var compGuess = letters[Math.round(Math.random(letters)*26)];
-
-// var userGuess = 
-
-// document.write
-
-// This section prints to the front
-
-document.getElementById("win").innerHTML = winCount;
-
-document.getElementById("loss").innerHTML = lossCount;
-
-document.getElementById("guessCount").innerHTML = guessEntry;
-
-document.getElementById("guesses").innerHTML = userBadGuess;
-
-// This section defines the basic variables
-var userGuess = document.onkeydown=function(event){
-    if (event.key.toUpperCase() === compGuess) {
-        winCount++;
-    }
-    if (event.key.toUpperCase() !== compGuess) {
-        guessEntry--;
-        document.getElementById('guesses').innerHTML=event.key;
-    } 
-    if (guessEntry===0) {
-        lossCount++;
-    }
-
-};
 
 var winCount = 0;
 
@@ -44,40 +14,42 @@ var guessEntry = 9;
 
 var userBadGuess = [];
 
-// document.onkeyup = function(event) { 
-        
-//         if (event.key !== 'compGuess') {
-
-//         	guessNum = guessNum - 1;
-
-//             document.getElementById("guessCount").innerHTML = guessNum;
-//         }
-
-//         else {
-//         	console.log('Nothing');
-//         }
-
-//     };
+var compGuess = letters[Math.round(Math.random(letters)*letters.length)];
 
 
+// This section defines the actions that occur when a user presses a key
+var userGuess = document.onkeyup=function(event){
 
+// This section should remove a remaining try from the total guesses
+    if (event.key.toUpperCase() !== compGuess) {
+        guessEntry-=1;
+        document.getElementById('guesses').innerHTML=event.key.toUpperCase();
+        document.getElementById('guessCount').innerHTML=guessEntry;
 
+// It should also add the key entered to the userBadGues array
 
+// This defines the reset condition if the player fails to guess right
+        if (guessEntry===0) {
+            document.getElementById('loss').innerHTML=lossCount+1;
+            document.getElementById('guessCount').innerHTML=9;
+        }
 
+    } 
+   
+// This section should say what should happen when a user gets it right
+    else if (event.key.toUpperCase() === compGuess) {
+    document.getElementById('win').innerHTML=winCount += 1;
+    document.getElementById('guessCount').innerHTML=guessEntry = 9;
+    document.getElementById('guesses').innerHTML="";
+    }
+};
 
+// This section prints the original variable counts to the front
 
+document.getElementById("win").innerHTML = winCount;
 
+document.getElementById("loss").innerHTML = lossCount;
 
+document.getElementById("guessCount").innerHTML = guessEntry;
 
-
-
-
-
-
-
-
-
-// for (i = 0; i < letters.length; i++) {
-// 	console.log((letters[i]));
-// }
-
+document.getElementById("guesses").innerHTML = userBadGuess;
