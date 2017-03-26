63109e31600c36cda1f71bf1844ad5921a8bd2ca
TREX Hughes <TREX@Traviss-MacBook-Pro-2.local>
2017-03-25 23:36:55 -0500

    Got everything working as described in the video. will be making final adjustments tomorrow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,31 +16,36 @@ var userBadGuess = [];
 
 var compGuess = letters[Math.round(Math.random(letters)*letters.length)];
 
-
 // This section defines the actions that occur when a user presses a key
 var userGuess = document.onkeyup=function(event){
 
 // This section should remove a remaining try from the total guesses
     if (event.key.toUpperCase() !== compGuess) {
         guessEntry-=1;
-        document.getElementById('guesses').innerHTML=event.key.toUpperCase();
+        userBadGuess.push(event.key);
+        document.getElementById('guesses').innerHTML=userBadGuess.join(', ');
         document.getElementById('guessCount').innerHTML=guessEntry;
 
 // It should also add the key entered to the userBadGues array
 
 // This defines the reset condition if the player fails to guess right
-        if (guessEntry===0) {
+        if (guessEntry==0) {
             document.getElementById('loss').innerHTML=lossCount+1;
             document.getElementById('guessCount').innerHTML=9;
+            guessEntry=9;
+            lossCount+=1;
+            userBadGuess=[];
+            compGuess = letters[Math.round(Math.random(letters)*letters.length)];
         }
 
     } 
    
 // This section should say what should happen when a user gets it right
     else if (event.key.toUpperCase() === compGuess) {
-    document.getElementById('win').innerHTML=winCount += 1;
-    document.getElementById('guessCount').innerHTML=guessEntry = 9;
-    document.getElementById('guesses').innerHTML="";
+        document.getElementById('win').innerHTML=winCount += 1;
+        document.getElementById('guessCount').innerHTML=guessEntry = 9;
+        document.getElementById('guesses').innerHTML="";
+        compGuess = letters[Math.round(Math.random(letters)*letters.length)];
     }
 };
 
